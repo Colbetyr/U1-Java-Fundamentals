3e01001214ff4819d9e4fbae683029215f7b8c1d
colbetyr000 <Colbetyr000@warren.k12.in.us>
2021-08-04 11:52:03 -0400

    Completed L1 L2 Notes

--- src/L1.java
+++ src/L1.java
@@ -1,8 +1,12 @@
 public class L1 {
 
-    //You will notice that the class header is already define.
+    public static void main(String[] args) {
 
-    //add the main method header below.
-
-
+        System.out.println("Hello World!!!!!!!!!!!1");
+        System.out.print("Hello");
+        System.out.print("World");
+        System.out.println("CS IS AWESOME!!!");
+        System.out.println("Tyrese Colbert");
+    }
 }
+
--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,22 @@
 public class L2 {
 
-    //add the main method header
+
+
+    public static void main(String[] args) {
+        String firstName = "Thomas Jefferson";
+        int age = 16;
+        System.out.println(firstName.toString());
+        System.out.println(age);
+
+        String customername = "Thomas Jefferson";
+        double balance = 13465447536.43;
+        int accountID = 848484884;
+
+        System.out.println(firstName.toString());
+        System.out.println("balance");
+        System.out.println(accountID);
+    }
 }
+
+
+
